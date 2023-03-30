4542c421168b978ef5ce913e388e02bccd0b01c2
haiweilian <haiweilian@foxmail.com>
2023-03-30 21:57:57 +0800

    chore: 更新子模块

--- vivy-modules/vivy-system/src/app.module.ts
+++ vivy-modules/vivy-system/src/app.module.ts
@@ -1,9 +1,38 @@
+import * as path from 'path'
+import { isArray, isObject } from 'lodash'
 import { Module } from '@nestjs/common'
+import { IpUtils } from '@vivycloud/common-core'
+import { ConfigModule, ConfigService } from '@vivycloud/config'
+import { NacosConfigModule, NacosConfigOptions, NacosNamingModule, NacosNamingOptions } from '@vivycloud/config-nacos'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      dir: path.resolve(__dirname, '../config'),
+    }),
+    NacosConfigModule.forRootAsync({
+      useFactory(configService: ConfigService) {
+        const config = configService.get<NacosConfigOptions>('nacos.config')
+        return config
+      },
+      inject: [ConfigService],
+    }),
+    NacosNamingModule.forRootAsync({
+      async useFactory(configService: ConfigService) {
+        const ip = await IpUtils.internalIpV4()
+        const naming = configService.get<NacosNamingOptions>('nacos.naming')
+        if (isArray(naming.instance)) {
+          naming.instance.forEach((ins) => (ins.ip = ip))
+        } else if (isObject(naming.instance)) {
+          naming.instance.ip = ip
+        }
+        return naming
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- vivy-modules/vivy-system/src/main.ts
+++ vivy-modules/vivy-system/src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core'
+import { ConfigService } from '@vivycloud/config'
+import { SwaggerService } from '@vivycloud/common-swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3040, () => {
-    console.log(`\n\n${'vivy-system'} run at http://localhost:${3040}/\n\n`)
+  const configService = app.get(ConfigService)
+
+  const swaggerService = new SwaggerService(app, {})
+  swaggerService.setup()
+
+  const port = configService.get<number>('application.port')
+  const name = configService.get<string>('application.name')
+  await app.listen(port, () => {
+    console.log(`\n\n${name} run at http://localhost:${port}/\n\n`)
   })
 }
 bootstrap()
